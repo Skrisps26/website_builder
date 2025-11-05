28c185752fcfd696f1fed2abfeb04f662ce93b3d
sai krishna <saikrishnasprasad2642005@gmail.com>
2025-11-06 02:14:48 +0530

    fixes

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_AWS_API_URL || "http://localhost:3001";
+const API_BASE_URL = process.env.NEXT_PUBLIC_AWS_API_URL;
 const API_KEY = process.env.NEXT_PUBLIC_AWS_API_KEY;
 
 interface ChatResponse {
